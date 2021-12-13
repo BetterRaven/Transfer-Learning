5c325247bf8808ce6288465863382e206667585c
Raven Stock <1034059311@qq.com>
2021-12-13 14:44:56 +0800

    bug fixed

--- tools/data_loader.py
+++ tools/data_loader.py
@@ -3,6 +3,10 @@ import numpy as np
 import scipy.io as scio
 from os.path import join
 import random
+from scipy.io import loadmat
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data.dataloader import DataLoader
+from sklearn.model_selection import train_test_split
 
 
 def load_data(root_path, sub_path, batch_size):
@@ -37,139 +41,12 @@ def load_data(root_path, sub_path, batch_size):
 
     return data, label
 
-
-def partial_loader(root_path, sub_path, num_target_classes=4, batch_size=64, shuffle=True):
-    """
-    根据给定的类别数量，加载部分迁移的数据
-    root_path:数据集的根目录
-    sub_path:指定目标域
-    num_target_classes:指定要加载的故障类型的类别数量
-    return:返回的是224*224的矩阵，resnet18要求的数据
-    """
-    data = scio.loadmat(join(root_path, sub_path, "all_data.mat"))
-    label = scio.loadmat(join(root_path, sub_path, "all_label.mat"))
-
-    data = data.get('feature')
-    label = label.get('label')
-    
-    # 随机选择一些类别
-    if shuffle:
-        classes_index = []  
-        while len(classes_index) != num_target_classes:
-            temp = random.randint(0, 9)
-            if temp not in classes_index:
-                classes_index.append(temp)
-
-    label = np.argmax(label, axis=1)
-    data_index = []
-    
-    # 根据classes_index选择相应的数据和标签
-    for index, item in enumerate(label):
-        if item in classes_index:
-            data_index.append(index)
-    temp = data[data_index]
-    label = label[data_index]
-
-    # 打乱数据
-    permutation = np.random.permutation(temp.shape[0])
-    temp = temp[permutation]                           
-    label = label[permutation]  
-
-    # 构建数据
-    data = None
-    temp = torch.tensor(temp)
-    data = torch.zeros((temp.shape[0], 224, 224))
-    for i in range(len(data)):
-        data[i] = temp[i].repeat(7, 7)
-
-    # 转为tensor
-    label = torch.tensor(label)
-    data = data.to(torch.float32)
-    data = torch.unsqueeze(data, dim=1)    # 添加一个维度，通道数
-    
-    data = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False, drop_last=True)
-    label = torch.utils.data.DataLoader(label, batch_size=batch_size, shuffle=False, drop_last=True)
-    return data, label
-
-def load_cwru(root, frequency, classes, hp, where, dtype='DE', batch_size=32):
-    """
-    功能：加载指定马力的数据
-    参数：root:数据集的根目录
-         frequency:使用什么平频率的数据，只能是12K或者48k
-         classes:每个子类的名称，默认4类（滚动体故障, 内圈故障, 外圈故障, 正常）
-         hp:负载值，只有0HP、1HP、2HP、3HP
-         where:使用风扇端还是驱动端
-         dtype:使用何种数据，只有DE、FE、BA
-         batch_size:批大小，默认32
-    返回：x_data:数据，类型为DataLoader，每个样本的大小为64*64
-         label:标签，类型为DataLoader
-    """
-    x_data = None
-    label = None
-    for index, item in enumerate(classes):
-        # 生成文件绝对路径
-        mat_path = join(root, frequency, item, where, hp)
-        files = os.listdir(mat_path)
-        
-        x_temp = None
-        for file in files:
-            mat = loadmat(join(mat_path, file))
-        
-            # 读取数据
-            temp = file.split('\\')[-1].split('.')[0]  # 获取文件名
-            if eval(temp) < 100:    # 仅用于处理97.mat -- 99.mat
-                temp = '0' + temp
-            key = 'X' + temp + '_' + dtype + '_time'
-            val = mat[key]
-            
-            # 将数据处理成64*64
-            for i in range(1, int(len(val))):
-                temp = val[(i - 1) * 4096 : i * 4096]
-                if len(temp) < 4096:
-                    break
-                # 标准化
-                mm = MinMaxScaler()
-                x_std = mm.fit_transform(temp.reshape((64, 64)))
-                if x_temp is not None:
-                    x_std = np.expand_dims(x_std, axis=0)
-                    x_temp = np.append(x_temp, x_std, axis=0)
-                else:
-                    x_temp = np.expand_dims(x_std, axis=0)
-                    
-        # 读完一类了，添加标签
-        if label is not None:
-            label = np.append(label, np.ones(x_temp.shape[0]) * index)
-        else:
-            label = np.ones(x_temp.shape[0]) * index
-        
-        # 拼合数据
-        if x_data is not None:
-            x_data = np.append(x_data, x_temp, axis=0)
-        else:
-            x_data = x_temp
-            
-    # 打乱数据
-    permutation = np.random.permutation(x_data.shape[0])
-    # 利用np.random.permutaion函数，获得打乱后的行数，输出permutation
-    x_data = x_data[permutation]
-    label = label[permutation]
-    
-    # 转化成DataLoader
-    x_data = torch.tensor(x_data)
-    x_data = x_data.to(torch.float32)
-    label = torch.tensor(label)
-    label = label.to(torch.long)
-    
-    x_data = torch.unsqueeze(x_data, dim=1)    # 添加一个维度，通道数
-    x_data = DataLoader(x_data, batch_size=batch_size, shuffle=False)
-    label = DataLoader(label, batch_size=batch_size, shuffle=False)
-    return x_data, label
-
-def load_dds(root, classes, dtype='AllYouData6', batch_size=32, test_size=0.1):
+def load_dds(root, classes, dtype='AllYouData6', batch_size=32, test_size=0.1, norm=True):
     """
     功能：加载DDS数据集
     参数：root:数据集的根目录
          classes:每个子类的名称，默认4类（滚动体故障, 内圈故障, 外圈故障, 正常）
+         hp:负载值，只有0HP、1HP、2HP、3HP
          dtype:使用何种数据，AllYouData6
          batch_size:批大小，默认32
          test_size:划分测试集的大小、
@@ -189,7 +66,7 @@ def load_dds(root, classes, dtype='AllYouData6', batch_size=32, test_size=0.1):
         # 读取数据
         key = dtype
         val = mat[key][0][0]
-        print(val.shape)
+        # print(val.shape)
         
         # 将数据处理成64*64
         for i in range(1, int(len(val))):
@@ -197,8 +74,12 @@ def load_dds(root, classes, dtype='AllYouData6', batch_size=32, test_size=0.1):
             if len(temp) < 4096:
                 break
             # 标准化
-            mm = MinMaxScaler()
-            x_std = mm.fit_transform(temp.reshape((64, 64)))
+            if norm:
+                mm = MinMaxScaler()
+                x_std = mm.fit_transform(temp.reshape((64, 64)))
+            else:
+                x_std = temp.reshape((64, 64))
+                
             if x_temp is not None:
                 x_std = np.expand_dims(x_std, axis=0)
                 x_temp = np.append(x_temp, x_std, axis=0)
@@ -251,3 +132,112 @@ def load_dds(root, classes, dtype='AllYouData6', batch_size=32, test_size=0.1):
         x_test = DataLoader(x_test, batch_size=batch_size, shuffle=False)
         y_test = DataLoader(y_test, batch_size=batch_size, shuffle=False)
     return x_train, x_test, y_train, y_test
+
+
+def partial_loader(root_path, sub_path, num_target_classes=4, batch_size=64, shuffle=True):
+    """
+    根据给定的类别数量，加载部分迁移的数据
+    root_path:数据集的根目录
+    sub_path:指定目标域
+    num_target_classes:指定要加载的故障类型的类别数量
+    """
+    data = scio.loadmat(join(root_path, sub_path, "all_data.mat"))
+    label = scio.loadmat(join(root_path, sub_path, "all_label.mat"))
+
+    data = data.get('feature')
+    label = label.get('label')
+    
+    # 随机选择一些类别
+    if shuffle:
+        classes_index = []  
+        while len(classes_index) != num_target_classes:
+            temp = random.randint(0, 9)
+            if temp not in classes_index:
+                classes_index.append(temp)
+
+    label = np.argmax(label, axis=1)
+    data_index = []
+    
+    # 根据classes_index选择相应的数据和标签
+    for index, item in enumerate(label):
+        if item in classes_index:
+            data_index.append(index)
+    temp = data[data_index]
+    label = label[data_index]
+
+    # 打乱数据
+    permutation = np.random.permutation(temp.shape[0])
+    temp = temp[permutation]                           
+    label = label[permutation]  
+
+    # 构建数据
+    data = None
+    temp = torch.tensor(temp)
+    data = torch.zeros((temp.shape[0], 224, 224))
+    for i in range(len(data)):
+        data[i] = temp[i].repeat(7, 7)
+
+    # 转为tensor
+    label = torch.tensor(label)
+    data = data.to(torch.float32)
+    data = torch.unsqueeze(data, dim=1)    # 添加一个维度，通道数
+    
+    data = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False, drop_last=True)
+    label = torch.utils.data.DataLoader(label, batch_size=batch_size, shuffle=False, drop_last=True)
+    return data, label
+
+def load_nyp(batch_size=64, test_size=0.1, shuffle=True):
+    """
+    加载npy文件，返回train_data，和test_data两个dataloader
+    参数:
+        batch_size:batch_size
+        test_size:测试集占总数据集的比例
+        shuffle:是否打乱数据
+    返回:train_data，和test_data两个dataloader
+    dataloader使用方式，请参考pytorch官方训练代码
+    """
+    x_data = np.load(r'E:\Raven\jupyter\Transfer Learning\Dataset\dds数据集\Data3232.npy')
+    label = np.load(r'E:\Raven\jupyter\Transfer Learning\Dataset\dds数据集\Label3232.npy')
+
+    if shuffle:
+    # 打乱数据
+        permutation = np.random.permutation(x_data.shape[0])
+        # 利用np.random.permutaion函数，获得打乱后的行数，输出permutation
+        x_data = x_data[permutation]
+        label = label[permutation]
+    
+    # 划分训练集和测试集
+    x_train, x_test, y_train, y_test = train_test_split(x_data, label, test_size=test_size)
+    
+    # 转化成DataLoader
+    x_train = torch.tensor(x_train)
+    x_train = x_train.to(torch.float32)
+    x_train = torch.unsqueeze(x_train, dim=1)    # 添加一个维度，通道数 
+    
+    x_test = torch.tensor(x_test)
+    x_test = x_test.to(torch.float32)
+    x_test = torch.unsqueeze(x_test, dim=1)      # 添加一个维度，通道数 
+    
+    y_train = torch.tensor(y_train)
+    y_train = y_train.to(torch.long)
+    
+    y_test = torch.tensor(y_test)
+    y_test = y_test.to(torch.long)
+
+
+    combined_train = []
+    for x, y in zip(x_train, y_train):
+        combined_train.append((x, y))
+
+    combined_test = []
+    for x, y in zip(x_test, y_test):
+        combined_test.append((x, y))
+
+    data_train = DataLoader(combined_train, batch_size=batch_size, shuffle=True)
+    if len(x_test) < batch_size:
+        data_test = DataLoader(combined_test, batch_size=len(x_test), shuffle=True)
+
+    else:
+        data_test = DataLoader(combined_test, batch_size=batch_size, shuffle=True)
+
+    return data_train, data_test
--- tools/mmd.py
+++ tools/mmd.py
@@ -31,12 +31,78 @@ def mmd_rbf_accelerate(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=N
 
 
 def mmd_rbf_noaccelerate(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
-    batch_size = int(source.size()[0])
+    # batch_size = int(source.size()[0])
+    # kernels = guassian_kernel(source, target,
+    #                           kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    # XX = kernels[:batch_size, :batch_size]
+    # YY = kernels[batch_size:, batch_size:]
+    # XY = kernels[:batch_size, batch_size:]
+    # YX = kernels[batch_size:, :batch_size]
+    # loss = torch.mean(XX + YY - XY -YX)
+    # return loss
+
+    source_num = int(source.size()[0])#一般默认为源域和目标域的batchsize相同
+    target_num = int(target.size()[0])
     kernels = guassian_kernel(source, target,
-                              kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
-    XX = kernels[:batch_size, :batch_size]
-    YY = kernels[batch_size:, batch_size:]
-    XY = kernels[:batch_size, batch_size:]
-    YX = kernels[batch_size:, :batch_size]
-    loss = torch.mean(XX + YY - XY -YX)
+        kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    #根据式（3）将核矩阵分成4部分
+    XX = torch.mean(kernels[:source_num, :source_num])
+    YY = torch.mean(kernels[source_num:, source_num:])
+    XY = torch.mean(kernels[:target_num, source_num:])
+    YX = torch.mean(kernels[source_num:, :target_num])
+    loss = XX + YY -XY - YX
+    return loss#因为一般都是n==m，所以L矩阵一般不加入计算
+
+# def multiple_mmd(feature, y_true, num_classes):
+#     # 获取每一类数据的下标
+#     class1 = []
+#     class2 = []
+#     class3 = []
+#     class4 = []
+#     for index, item in enumerate(y_true):
+#         if item == 0:
+#             class1.append(index)
+#         elif item == 1:
+#             class2.append(index)
+#         elif item == 2:
+#             class3.append(index)
+#         else:
+#             class4.append(index)
+            
+#     # 将每一类特征分开
+#     loss = 0
+#     feature_mmd = [feature[class1], feature[class2], feature[class3], feature[class4]]
+    
+#     # 计算MMD距离
+#     for i in range(len(feature_mmd)):
+#         for j in range(i+1, len(feature_mmd)):
+#             loss += mmd_rbf_noaccelerate(feature_mmd[i], feature_mmd[j])
+            
+#     return loss
+
+def multiple_mmd(feature, y_pred, y_true, num_classes=8):
+    # 获取每一类数据的下标
+    classes = []
+    for i in range(num_classes):
+        classes.append([])
+
+    for index, item in enumerate(y_true):
+        classes[item].append(index)
+            
+    # 将每一类特征分开
+    loss = 0
+    losses = []
+    feature_mmd = []
+
+    for item in classes:
+        feature_mmd.append(feature[item])
+    
+    # 计算MMD距离
+    for i in range(len(feature_mmd)):
+        for j in range(i+1, len(feature_mmd)):
+            loss += mmd_rbf_noaccelerate(feature_mmd[i], feature_mmd[j])
+    
+    # ---------------计算每一类的分类准确率---------------
+
+            
     return loss
